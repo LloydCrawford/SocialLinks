d44715d54222d66790810f39bfc32b4779feaa09
LloydCrawford <56405324+LloydCrawford@users.noreply.github.com>
2020-02-09 21:11:13 +0000

    1

--- scripts.js
+++ scripts.js
@@ -5,5 +5,5 @@ function changePic() {
   }
   
   function normalPic() {
-    document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
+    document.getElementById("profilePic") .src = "Images/lloyd_crawford2.jpg";
   }
